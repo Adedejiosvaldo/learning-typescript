67c3d0cb2745d2dc7a14a4d87c21c017be6dd152
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-16 10:28:14 +0100

    Function with type inference

--- features/annotations/function.ts
+++ features/annotations/function.ts
@@ -1,3 +1,10 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+// When there is no annotation for the returning value
+// typescript infers a type for us
+
+const addWithNoInference = (a: number, b: number) => {
+  return a + b;
+};
