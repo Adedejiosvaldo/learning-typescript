f761adaa9841cfd2b96126e909e45c1a77286b8a
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-16 10:33:09 +0100

    Anon function

--- features/annotations/function.ts
+++ features/annotations/function.ts
@@ -4,7 +4,16 @@ const add = (a: number, b: number): number => {
 
 // When there is no annotation for the returning value
 // typescript infers a type for us
-
 const addWithNoInference = (a: number, b: number) => {
   return a + b;
+};
+
+// Anon function
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+// assigning a function to a variable
+const multiply = function (a: number, b: number): number {
+  return a * b;
 };
