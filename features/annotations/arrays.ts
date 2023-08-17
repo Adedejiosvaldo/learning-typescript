80abcbca3aa0352c1cd1abe268da4bb9432e9280
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-17 05:52:34 +0100

    Flexible array

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -6,3 +6,12 @@ const carMakers: string[] = ["ford", "toyota", "Dodge"];
 const date = [new Date()];
 
 const carsByMake: string[][] = [[], ["Ferrari"], ["Camero"]];
+
+// helps with inference when extracting values
+const cars = carMakers[0];
+
+// Prevents incompatible values
+// carMakers.push(100);
+
+// FLexible Arrays
+const dateTime: (Date | string)[] = [new Date(), "Hello"];
