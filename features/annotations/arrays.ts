bcc10d274107cfc70b308f33678f2610865e9339
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-17 05:43:32 +0100

    array types

---
+++ features/annotations/arrays.ts
@@ -0,0 +1,8 @@
+// arrays where each element is consistent
+
+const carMakers: string[] = ["ford", "toyota", "Dodge"];
+// Complex objects inside arrays
+
+const date = [new Date()];
+
+const carsByMake: string[][] = [[], ["Ferrari"], ["Camero"]];
