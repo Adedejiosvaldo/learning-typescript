8ec2cb28b5f05e11f19394aeecec259d5543c8a1
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-19 07:00:09 +0100

    added more interface features

--- features/annotations/interface.ts
+++ features/annotations/interface.ts
@@ -1,15 +1,21 @@
 const oldCivic = {
   name: "Civic",
   year: 2000,
-  broken: true,
+  broken: true, 
+  summary(): string {
+    return this.name;
+  },
 };
 
 interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
 }
 
 const fetchFUnction = (vehicle: Vehicle): void => {
   console.log(vehicle.broken);
 };
+
+fetchFUnction(oldCivic);
