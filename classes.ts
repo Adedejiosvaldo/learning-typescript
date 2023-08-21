99db40aeb9d1a91cc288b106803b582950905f1e
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-21 09:35:45 +0100

    Added class modifiers

--- classes.ts
+++ classes.ts
@@ -23,3 +23,25 @@ class Car extends Vehicle {
 const vehicle = new Car();
 vehicle.drive();
 vehicle.honk();
+
+// Modifier (they restrict access) -> PUblic,private and protected
+// PUblic -> This method can be called anywhere,any time
+// Private -> THis method can only be called by other methods in the class
+// Protected -> This method can be called onl
+
+class House {
+  public build(): void {
+    console.log("Get Building");
+  }
+
+  private moveIn(): void {
+    console.log("Yay ,Home is complete");
+  }
+
+  familyMoveIn(): void {
+    this.moveIn();
+  }
+}
+
+const duplex = new House();
+duplex.familyMoveIn();
